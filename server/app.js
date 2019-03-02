47b465d0d24cb56ad22fbbb3ff02819c836cf8fc
vishalseshagiri <vishaljsesh@gmail.com>
2019-03-02 04:40:47 -0800

    adds working uber code

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const firebase = require('firebase');
+require('dotenv').config();
 
 const app = express();
 
--- server/routes/api/uber.js
+++ server/routes/api/uber.js
@@ -1,13 +1,12 @@
 const router = require('express').Router();
 var Uber = require('node-uber');
-require('dotenv').config()
 
 var uber = new Uber({
   client_id: process.env.CLIENT_ID,
   client_secret: process.env.CLIENT_SECRET,
   server_token: process.env.SERVER_TOKEN,
   redirect_uri: 'https://localhost:8000/callback',
-  name: 'FUND_MY_CAUSE'
+  name: 'hacktech2019_usc'
 //   language: 'en_US', // optional, defaults to en_US
 //   sandbox: true, // optional, defaults to false
 //   proxy: 'PROXY URL' // optional, defaults to none
@@ -15,37 +14,41 @@ var uber = new Uber({
 
 router.get('/', (req, res, next) => {
     // Kick off the authentication process
+    console.log(uber);
     var scope = ['request', 'history', 'profile', 'places'];
     redirect_uri = uber.getAuthorizeUrl(scope, 'http://localhost:8000/api/uber/callback');
     console.log(redirect_uri)
-    res.redirect(redirect_uri);
+    res.redirect('http://localhost:8000/api/uber/callback');
 });
 
-router.get('/callback', function(request, response) {
-    console.log('inside callback')
-    uber.authorizationAsync({authorization_code: request.query.code})
-    .spread(function(access_token, refresh_token, authorizedScopes, tokenExpiration) {
-      // store the user id and associated access_token, refresh_token, scopes and token expiration date
-      console.log('New access_token retrieved: ' + access_token);
-      console.log('... token allows access to scopes: ' + authorizedScopes);
-      console.log('... token is valid until: ' + tokenExpiration);
-      console.log('... after token expiration, re-authorize using refresh_token: ' + refresh_token);
+// router.get('/callback', function(request, response) {
+//     console.log('inside callback')
+//     uber.authorizationAsync({authorization_code: request.query.code})
+//     .spread(function(access_token, refresh_token, authorizedScopes, tokenExpiration) {
+//       // store the user id and associated access_token, refresh_token, scopes and token expiration date
+//       console.log('New access_token retrieved: ' + access_token);
+//       console.log('... token allows access to scopes: ' + authorizedScopes);
+//       console.log('... token is valid until: ' + tokenExpiration);
+//       console.log('... after token expiration, re-authorize using refresh_token: ' + refresh_token);
 
-      // redirect the user back to your actual app
-    //   response.redirect('/web/index.html');
-    })
-    .error(function(err) {
-      console.error(err);
-    });
-});
-
-// router.get('/callback', (req, res) => {
-//     uber.authorization({grantType: 'authorization_code', authorization_code: req.query.code}, function (err, access_token) {
-//         uber.access_token = access_token;
-//         console.log('Got an access token! Head to /book to initiate an ride request.');
-//         res.send('Got an access token! Head to /book to initiate an ride request.');
+//       // redirect the user back to your actual app
+//     //   response.redirect('/web/index.html');
+//     })
+//     .error(function(err) {
+//       console.error(err);
 //     });
-
 // });
+
+router.get('/callback', function (req, res) {
+  uber.authorization ({grantType: 'authorization_code', authorization_code: req.query.code}, function (err, access_token) {
+    // Now we've got an access token we can use to book rides.
+    // Access tokens expires in 30 days at whichpoint you can refresh.
+    // You should save this token
+    // More info: https://developer.uber.com/docs/authentication
+    uber.access_token = access_token;
+    console.log(uber.access_token);
+    res.send('Got an access token! Head to /book to initiate an ride request.');
+  });
+});
 
 module.exports = router;
