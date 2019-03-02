c64609e62c01f130487a4a1a4ffa079673a937e6
Bala <bsthiagarajan@gmail.com>
2019-03-02 15:50:03 -0800

    added walmart_scraper

---
+++ ebay_walmart/walmart_spider.py
@@ -0,0 +1,41 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+wprice = 0
+# Scrapy Spider definition
+class RedSpider(scrapy.Spider):
+    name = "red"
+    def __init__(self, url):
+        start_urls = [url]
+        # seed URL
+        self.start_urls = start_urls
+
+    # Main parser
+    def parse(self, response):
+        global wprice
+        name = ' '.join(response.xpath('//*[@id="searchProductResult"]/ul/li[1]/div/div[2]/div[5]/div/span[2]/a/span/mark/text()').extract())
+        wprice = response.xpath('//*[@id="searchProductResult"]/ul/li[1]/div/div[2]/div[7]/div/span/div/div/div/span/span/span[2]/text()')
+        if(name == '' or name == ' '):
+            name = ' '.join(response.xpath('//*[@id="searchProductResult"]/div/div[1]/div/div/div[2]/div[2]/div[1]/div[2]/span[2]/a/span/mark/text()').extract())
+            wprice = response.xpath('//*[@id="searchProductResult"]/div/div[1]/div/div/div[2]/div[2]/div[2]/span/div/div/div/span/span/span[2]/text()').extract_first()
+
+def mainf(url):
+    # Crawler Settings
+    process = CrawlerProcess({
+        'USER_AGENT': 'Mozilla/5.0',
+        'ROBOTSTXT_OBEY' : False,
+        'AUTOTHROTTLE_ENABLED' : True, # Enables AutoThrottle
+        'COOKIES_ENABLED' : False,
+        'LOG_LEVEL' : 'INFO'
+        #'FEED_FORMAT': 'csv', # Output Format
+        #'FEED_URI': 'data.csv' # Output File
+    })
+
+    process.crawl(RedSpider, url = url)
+    process.start() # Start crawling
+    return wprice
+
+if __name__ == '__main__':
+    url = 'https://www.walmart.com/search/?query=expensive%20camp%20tent%20'
+    wprice = mainf(url)
+    print("Walmart Price:",wprice)
